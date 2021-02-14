630d10b43dfa2f8740d22cbb93e6ef94eaf4f9e1
amemaj92 <64326280+amemaj92@users.noreply.github.com>
2021-02-14 18:49:09 +0100

    Update sort.php

--- series/sort.php
+++ series/sort.php
@@ -7,7 +7,7 @@ if(isset($_POST["simple_search"])) {
         clearSortKeyFromSession();
         $sort_key = "def";
 }
-else if(isset($_POST["Sort"])) $sort_key = $_POST["Sort"];		//Post value of Sort key has precedence
+elseif(isset($_POST["Sort"])) $sort_key = $_POST["Sort"];		//Post value of Sort key has precedence
 elseif(isset($SESSION["Sort"])) $sort_key=$SESSION["Sort"]; //Then get value of sort key saved in session
 else $sort_key="def";  										//Otherwise set to default value. 
 setSortColumns ($sort_key); 
