009371ec07f527974e38f9ffce143d9cdd4b4dc5
amemaj92 <64326280+amemaj92@users.noreply.github.com>
2021-02-14 18:47:44 +0100

    Update sort.php

--- series/sort.php
+++ series/sort.php
@@ -1,10 +1,13 @@
 <?php
 
 include_once("sort_functions.php");
-//Setting Sort variables. 
-$Sort_Col; $Sort_ASC_DESC; 
 //Initial Value of Sorting variable $sort_key; 
-if(isset($_POST["Sort"])) $sort_key = $_POST["Sort"];		//Post value of Sort key has precedence
+if(isset($_POST[$_POST["simple_search"])) { 
+        //Clearing previous sort key from session and setting it to default
+        clearSortKeyFromSession();
+        sort_key = "def";
+}
+else if(isset($_POST["Sort"])) $sort_key = $_POST["Sort"];		//Post value of Sort key has precedence
 elseif(isset($SESSION["Sort"])) $sort_key=$SESSION["Sort"]; //Then get value of sort key saved in session
 else $sort_key="def";  										//Otherwise set to default value. 
 setSortColumns ($sort_key); 
