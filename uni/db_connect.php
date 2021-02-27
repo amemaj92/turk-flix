7f86c34923e9cdb18395f654c02d8e7f0bc8ec5b
amemaj92 <64326280+amemaj92@users.noreply.github.com>
2021-02-27 15:32:13 +0100

    Add files via upload

---
+++ uni/db_connect.php
@@ -0,0 +1,12 @@
+<?php 
+
+include('Uni_Globals.php');   
+include('Uni_Functions.php');
+
+
+$VID_SERIES = new mysqli(HOST, USER1, PASSWORD1, DATABASE1);
+$VID_MOVIES = new mysqli(HOST, USER1, PASSWORD1, DATABASE2);
+
+
+?>
+
